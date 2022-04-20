04e9ae638d1fc28d4a773a49ce74b5c3c3bf72ae
Vitali_Santalau <v.santalau@gmail.com>
2022-04-21 01:17:23 +0300

    feat: add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
